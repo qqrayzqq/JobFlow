04efd29a6878c7e342ca8426f2a4c263e725aece
qqrayzqq <162329881+qqrayzqq@users.noreply.github.com>
2026-06-22 00:04:41 +0200

    feat: initial project setup - job-service skeleton

---
+++ jobflow-job-service/src/main/java/com/jobflow/jobservice/JobflowJobServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jobflow.jobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobflowJobServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobflowJobServiceApplication.class, args);
+    }
+
+}
---
+++ jobflow-job-service/src/test/java/com/jobflow/jobservice/JobflowJobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobflow.jobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobflowJobServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
