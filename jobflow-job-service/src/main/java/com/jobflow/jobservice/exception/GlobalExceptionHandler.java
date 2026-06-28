c88c45e614c2a2b249e0731d4676b89beed49bdf
qqrayzqq <162329881+qqrayzqq@users.noreply.github.com>
2026-06-28 23:11:11 +0200

    feat: add catch-all exception handler with error logging

--- jobflow-job-service/src/main/java/com/jobflow/jobservice/exception/GlobalExceptionHandler.java
+++ jobflow-job-service/src/main/java/com/jobflow/jobservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jobflow.jobservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -38,5 +40,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RateLimitExceededException.class)
     public ResponseEntity<?> handleRateLimit(RateLimitExceededException ex) {
         return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAllOtherExceptions(Exception ex){
+        log.error("Unexpected error", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
     }
 }
